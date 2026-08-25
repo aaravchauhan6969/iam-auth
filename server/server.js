37803efe2504c2b0205da1b60f147362920f06e2
aaravchauhan6969 <a2zaaravx2024@gmail.com>
2026-08-25 09:14:18 +0530

    Prepare apoplicartion for deployement

--- server/server.js
+++ server/server.js
@@ -600,10 +600,8 @@ app.post("/api/verify-sms-otp", (req, res) => {
 
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log(
-        "Server running on http://localhost:5000"
-    );
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
